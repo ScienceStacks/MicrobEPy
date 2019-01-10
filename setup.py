fdbc78a0c2dfa445a23618be56aa4219b51f2fb2
Joe Hellerstein <jlheller@uw.edu>
2019-01-09 16:21:15 -0800

    Handling install requirements and check for conda.

--- setup.py
+++ setup.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import distutils.core
 import os
+import subprocess
 import shutil
 
 
 DATA_DIR = "data_base"
 PROJECT_NAME = "microbepy"
 DB_NAME = "%s.db" % PROJECT_NAME
+REQUIREMENTS_FILE = "requirements.txt"
+
+# Install required packages
+def doRequirements():
+  with open(REQUIREMENTS_FILE) as fd:
+    requirements = fd.readlines()
+  for requirement in requirements:
+    pkg = requirement.replace('\n', '')
+    subprocess.call(["pip", "install", pkg])
 
 # Copy data to install directory
 def copyData():
   curdir = os.path.realpath(os.curdir)
   dst = os.path.join(curdir, PROJECT_NAME)
   dst = os.path.join(dst, DATA_DIR)
-  if not os.path.isfile(dst):
+  if not os.path.isdir(dst):
     os.mkdir(dst)
   dst = os.path.join(dst, DB_NAME)
   src = os.path.join(curdir, "Data")
@@ -24,12 +34,30 @@ def copyData():
     raise ValueError("Cannot file microbepy.db")
   shutil.copyfile(src, dst)
 
+# Test to see if conda has been installed
+def isCondaInstalled():
+  result = True
+  try:
+    subprocess.call(["conda"], stdout=subprocess.DEVNULL)
+  except:
+    result = False
+  return result
+
+# Main logic
+def main():
+  if isCondaInstalled():
+    copyData()
+    doRequirements()
+    distutils.core.setup(name='microbepy',
+        version='1.0',
+        description='Python support for analysis of Microbial Communities',
+        author='Joseph Hellerstein',
+        author_email='jlheller@uw.edu',
+        packages=['microbepy'],
+        package_data={'': ['*.db']},
+        )
+  else:
+    print("***No conda installation detected. Please install.")
+
 if __name__ == '__main__':
-  copyData()
-  setup(name='microbepy',
-      version='1.0',
-      description='Python support for analysis of Microbial Communities',
-      author='Joseph Hellerstein',
-      author_email='jlheller@uw.edu',
-      packages=['microbepy'],
-      )
+  main()
--- test_setup.py
+++ test_setup.py
@@ -1,4 +1,5 @@
 import setup
+import distutils.core
 
 import shutil
 import sys
@@ -13,7 +14,21 @@ class TestFunctions(unittest.TestCase):
   @mock.patch('os.mkdir', return_value=mock.Mock())
   @mock.patch('shutil.copyfile', return_value=mock.Mock())
   def testCopyData(self, _, __):
+    # Smoke test
     setup.copyData()
+
+  @mock.patch('subprocess.call', return_value=mock.Mock())
+  def testDoRequirements(self, _):
+    # Smoke test
+    setup.doRequirements()
+
+  @mock.patch('os.mkdir', return_value=mock.Mock())
+  @mock.patch('shutil.copyfile', return_value=mock.Mock())
+  @mock.patch('subprocess.call', return_value=mock.Mock())
+  @mock.patch('distutils.core.setup', return_value=mock.Mock())
+  def testMain(self, _, __, ___, ____):
+    # Smoke test
+    setup.main()
 
 
 if __name__ == '__main__':
