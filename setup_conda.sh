1278185517f4b9d3bc0b09a89e581fcf8853d559
ubuntu <jlheller@uw.edu>
2019-01-10 17:45:39 -0800

    setup for conda

--- setup_conda.sh
+++ setup_conda.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-# Does setup after miniconda is installed
-CONDA=$HOME/miniconda3/bin/conda
-# Do Conda installs
-$CONDA update -n base conda
-$CONDA install python=3.6.4  # Latest release for Tellurium
-$CONDA install numpy
-$CONDA install pandas
-$CONDA install matplotlib
-$CONDA install jupyter notebook
-$CONDA install scikit-learn
-#
-sudo apt install python-setuptools
-# Other pip installs
-pip install xlrd
-pip install nose
-pip install coverage
-pip install pyyaml
+# Set-up miniconda
+PY=3
+CONDA=$HOME/miniconda${PY}/bin/conda
+rm -rf $HOME/miniconda${PY}
+wget https://repo.continuum.io/miniconda/Miniconda${PY}-latest-Linux-x86_64.sh
+bash Miniconda${PY}-latest-Linux-x86_64.sh
+rm Miniconda${PY}-latest-Linux-x86_64.sh
